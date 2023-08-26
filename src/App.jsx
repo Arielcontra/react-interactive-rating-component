2be1c67a7a58c051d71cbca8e485cc4f55673a2d
Ariel Contreras <arielcontra@hotmail.com>
2023-08-25 21:20:50 -0300

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import * as Screen from './components'
+import * as Screen from './components/index.jsx'
 import './App.css'
 import './estilos/reset.css'
 
